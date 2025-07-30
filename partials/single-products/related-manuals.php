420d645b0e50bdf169eea63d6080975533ced72c
Darinel Nieto <darinelnieto@Darinels-MacBook-Pro.local>
2025-07-30 08:43:05 -0500

    Contact

--- partials/single-products/related-manuals.php
+++ partials/single-products/related-manuals.php
@@ -8,7 +8,89 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
-$manuals = get_field('')
+$manuals = get_field('related_manuals');
+if($manuals['manuals']):
 ?>
-<section class="related-manuals-partial-d5f0ac"></section>
-                    
+<section class="related-manuals-partial-d5f0ac">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2><?= $manuals['title']; ?></h2>
+                <div class="related-manuals-contain">
+                    <div class="mauals-slide owl-carousel">
+                        <?php foreach($manuals['manuals'] as $item): ?>
+                            <div class="item">
+                                <a href="<?= get_permalink($item->ID); ?>" class="card-manual">
+                                    <img src="<?= get_the_post_thumbnail_url($item->ID); ?>" alt="<?= get_the_title($item->ID); ?>" width="350" height="190" class="feature-image">
+                                    <div class="text-contain">
+                                        <h3><?= get_the_title($item->ID); ?></h3>
+                                        <p class="description"><?= get_field('short_description', $item->ID); ?></p>
+                                    </div>
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script>
+    $('.mauals-slide').owlCarousel({
+        loop:false,
+        autoplay:false,
+        nav:true,
+        navText:[
+            `<svg width="68" height="69" viewBox="0 0 68 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_142_196)">
+                    <circle cx="34" cy="30.5" r="30" transform="rotate(180 34 30.5)" fill="white" fill-opacity="0.7"/>
+                    <path d="M38 22.5L30 30.5L38 38.5" stroke="#1A1A1A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                <defs>
+                    <filter id="filter0_d_142_196" x="0" y="0.5" width="68" height="68" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dy="4"/>
+                        <feGaussianBlur stdDeviation="2"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0.470588 0 0 0 0 0.470588 0 0 0 0 0.470588 0 0 0 0.05 0"/>
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_142_196"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_142_196" result="shape"/>
+                    </filter>
+                </defs>
+            </svg>`,
+            `<svg width="68" height="69" viewBox="0 0 68 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_142_264)">
+                    <circle cx="34" cy="30.5" r="30" fill="white" fill-opacity="0.7"/>
+                    <path d="M29 38.5L37 30.5L29 22.5" stroke="#1A1A1A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                <defs>
+                    <filter id="filter0_d_142_264" x="0" y="0.5" width="68" height="68" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dy="4"/>
+                        <feGaussianBlur stdDeviation="2"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0.470588 0 0 0 0 0.470588 0 0 0 0 0.470588 0 0 0 0.05 0"/>
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_142_264"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_142_264" result="shape"/>
+                    </filter>
+                </defs>
+            </svg>`
+        ],
+        dots:true,
+        margin:20,
+        responsive:{
+            0:{
+                items:1
+            },
+            640:{
+                items:2
+            },
+            991:{
+                items:3
+            }
+        }
+    });
+</script>
+<?php endif; ?> 
--- templates/contact-us-template.php
+++ templates/contact-us-template.php
@@ -9,92 +9,63 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 get_header();
-$banner = get_field('background_image');
+$after_form = get_field('after_form_content');
 ?>
 <main id="contact-us-template-55cc3c">
-    <section>
-        <img src="<?= $banner['url']; ?>" alt="<?= $banner['title']; ?>" class="background-image">
+    <section class="contact-content">
         <div class="container">
-            <div class="row" id="content-contact">
-                <div class="col-12 col-md-6">
-                    <h1 class="title-page" data-aos="fade-right"><?= get_field('title_page'); ?></h1>
-                    <p class="desription-page" data-aos="fade-right"><?= get_field('description_page'); ?></p>
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-content">
+                        <p class="text-before-title"><?= get_field('text_before_title'); ?></p>
+                        <h1><?= get_field('title'); ?></h1>
+                    </div>
+                    <div class="form">
+                        <?= do_shortcode(get_field('shortcode_form')); ?>
+                    </div>
                 </div>
-                <div class="col-12 col-md-6">
-                    <div class="from-card" data-aos="fade-left" >
-                        <h2><?= get_field('title_form_contact'); ?></h2>
-                        <p><?= get_field('intro_form_contact'); ?></p>
-                        <div class="form">
-                            <?= do_shortcode(get_field('shortcode_form')); ?>
+            </div>
+        </div>
+    </section>
+    <section class="after-form">
+        <img src="<?= $after_form['main_image']['url']; ?>" alt="<?= $after_form['main_image']['title']; ?>" width="<?= $after_form['main_image']['width']; ?>" height="<?= $after_form['main_image']['height']; ?>">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-contain">
+                        <div class="left-content">
+                            <p class="before-title"><?= $after_form['text_before_title']; ?></p>
+                            <h2><?= $after_form['title']; ?></h2>
+                        </div>
+                        <div class="right-contain">
+                            <div class="content">
+                                <h3><?= $after_form['contact_us_title']; ?></h3>
+                                <div class="description">
+                                    <?= $after_form['contact']; ?>
+                                </div>
+                            </div>
+                            <div class="content">
+                                <h3><?= $after_form['location_title']; ?></h3>
+                                <div class="description">
+                                    <?= $after_form['location']; ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
-    <section class="pqr-section">
-        <div class="container">
-            <div class="pqr-content">
-                <div class="pqr-left" data-aos="fade-right">
-                    <h3 class="pqr-title"><?= get_field('pqr_left_content')['title']; ?></h3>
-                    <h4 class="pqr-subtitle"><?= get_field('pqr_left_content')['subtitle']; ?></h4>
-                    <ul class="pqr-list">
-                        <?php if (have_rows('pqr_left_content')) : 
-                            while (have_rows('pqr_left_content')) : the_row();
-                                if (have_rows('contact_list')) :
-                                    while (have_rows('contact_list')) : the_row(); ?>
-                                        <li>
-                                            <span><?= get_sub_field('icon'); ?></span>
-                                            <a href="<?= get_sub_field('link')['url']; ?>"><?= get_sub_field('link')['title']; ?></a>
-                                        </li>
-                                    <?php endwhile;
-                                endif;
-                            endwhile;
-                        endif; ?>
-                    </ul>
-                </div>
-
-                <div class="pqr-right" data-aos="fade-left">
-                    <p><?= get_field('pqr_right_content')['intro']; ?></p>
-                    <img src="<?= get_field('pqr_right_content')['image']['url']; ?>" alt="<?= get_field('pqr_right_content')['image']['title']; ?>">
-                    <h3 class="pqr-title"><?= get_field('pqr_right_content')['pqrsf_title']; ?></h3>
-                    <ul class="pqr-list">
-                        <?php if (have_rows('pqr_right_content')) : 
-                            while (have_rows('pqr_right_content')) : the_row();
-                                if (have_rows('pqrsf_list')) :
-                                    while (have_rows('pqrsf_list')) : the_row(); ?>
-                                        <li>
-                                            <div class="li-img">
-                                                <img src="<?= get_sub_field('icon')['url']; ?>" alt="<?= get_sub_field('icon')['title']; ?>">
-                                            </div>
-                                            <div class="pqr-text">
-                                                <strong><?= get_sub_field('name'); ?></strong>
-                                                <span><?= get_sub_field('description'); ?></span>
-                                            </div>
-                                        </li>
-                                    <?php endwhile;
-                                endif;
-                            endwhile;
-                        endif; ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
 </main>
 <script>
-    $(document).ready(function(){
-        $('.button-container .icon').html(`
-            <svg xmlns="http://www.w3.org/2000/svg" width="15.656" height="15.656" viewBox="0 0 15.656 15.656">
-                <g id="Grupo_240" data-name="Grupo 240" transform="translate(-1539.091 -986.869)">
-                    <line id="Línea_13" data-name="Línea 13" y1="14.802" x2="14.802" transform="translate(1539.444 987.369)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="1"/>
-                    <line id="Línea_14" data-name="Línea 14" x2="11.698" transform="translate(1542.548 987.369)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="1"/>
-                    <line id="Línea_15" data-name="Línea 15" y2="11.698" transform="translate(1554.246 987.369)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="1"/>
-                </g>
+    $(()=>{
+        $('.form button .icon').html(`
+            <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 12.4648H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 5.46484L19 12.4648L12 19.4648" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
         `);
-    });
+    })
 </script>
 <?php get_footer(); ?>
                     
